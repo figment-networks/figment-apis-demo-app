338869c9465b78ec83afc9e2c02e6e6ea14096a7
Rogan X <rogan@figment.io>
2022-11-23 16:42:25 -0700

    Update from development

--- pages/index.js
+++ pages/index.js
@@ -12,28 +12,38 @@ export default function Home() {
   const { loaded, accountId, publicKey, secretKey, available, staked } = appState;
 
   /**
-   * creates a randomly generated NEAR testnet account
-   * @returns {object} - accountId, publickey, secretKey
+   * createAccount
+   * @description creates a new near protocol account 
+   * @modifies {object} appState - sets the accountId, publickey, secretKey for the new account
+   * @throws {Response} throws response object if the createAccount request fails 
    */
   async function createAccount() {
+    // create account    
     const response = await fetch("/api/createAccount", {
       headers: { "Content-Type": "application/json" },
       method: "POST",
     });
 
-    if (!response.ok) throw Error(response.statusText);
+    if (!response.ok) throw new Error(response);
+
+    // update the app state with the accountId and keypair
     const { accountId, publicKey, secretKey } = await response.json();
     setAppState({ accountId, publicKey, secretKey });
   }
 
   /**
-   * delegates tokens to a node for staking
+   * delegate
+   * @description delegates a given amount of tokens to a node for staking
+   * @param {float} amount - the amount of NEAR to be staked
+   * @callback waitForDelegation - waits for broadcasted tx to reach delegated state
+   * @callback updateBalances - runs after broadcasted tx is delegated updating appState.available && appState.staked
+   * @modifies {object} appState - updates the available && staked balances via updateBalances
+   * @throws {Response} throws response object if the delegateFlow, submitData or broadcastTx requests fails 
    */
   async function delegate(amount) {
-    let response
+    let response;
 
-    setAppState({ ...appState });
-
+    // create the flow
     response = await fetch("/api/delegateFlow", {
       headers: { "Content-Type": "application/json" },
       method: "POST",
@@ -45,8 +55,10 @@ export default function Home() {
       })
     });
 
-    const { id: flow_id, actions } = await response.json()
-    const validator_address = 'legends.pool.f863973.m0'
+    if (!response.ok) throw new Error(response);
+    
+    // submit the data
+    const { id: flow_id, actions } = await response.json();
 
     response = await fetch("/api/submitData", {
       method: "POST",
@@ -57,18 +69,21 @@ export default function Home() {
         inputs: {
           delegator_address: appState.accountId,
           delegator_pubkey: appState.publicKey,
-          validator_address: validator_address,
+          validator_address: 'legends.pool.f863973.m0',
           amount,
+          max_gas: null
         }
       })
-    })
+    });
 
-    if (!response.ok) alert('yo')
+    if (!response.ok) throw new Error(response);
 
+    // sign the transaction
     const { data } = await response.json();
     // const transaction_payload = data.delegate_transaction.raw;
     const signed_payload = await slate.sign("near", "v1", transaction_payload, [secretKey]);
 
+    // broadcast the signed transaction
     response = await fetch('/api/broadcastTx', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
@@ -78,61 +93,74 @@ export default function Home() {
       })
     });
 
-    if (!response.ok) alert('yo');
+    if (!response.ok) throw new Error(response);
 
+    // wait for delegated state
     const { id } = await response.json();
-
     await waitForDelegation(id);
+
+    // update the balances
     await updateBalances();
   }
 
   /**
-   * updates the available balance to stake and current staked balance
+   * updateBalances 
+   * @description updates the available and staked balances in the appState
+   * @modifies {object} appState - updates staked and available properties with current balances
+   * @throws {Response} throws response object if the getNearBalance request fails 
    */
   async function updateBalances() {
-    if (!accountId) return
-    const result = await fetch('/api/getNearBalance?account=' + accountId)
-    const json = await result.json()
-    const { available, staked } = json
+    if (!accountId) return;
+    const response = await fetch('/api/getNearBalance?account=' + accountId);
+    if (!response.ok) throw new Error(response);
+
+    const { available, staked } = await response.json();
     const update = {
       available: parseFloat((parseFloat(available) / DECIMALS).toFixed()),
       staked: parseFloat((parseFloat(staked) / DECIMALS).toFixed()),
-    }
+    };
 
     setAppState({
       ...appState,
       ...update,
     });
-
-    return update
   }
 
   /**
-   * checks the state of a flow
-   *  - when the transaction has been broadcast and is awaiting conformation the state is "delegate_tx_broadcast"
-   *  - when the transaction is confirmed on-chain the state becomes "delegated"
+   * waitForDelegation
+   * @description returns a Promise that runs every five seconds, resolving when a transaction state is shown to be delegated
+   * @param {string} txId - the transaction id for the transaction to monitor for delegate state
+   * @param {function} resolve - the resolve function used to finalize initial Promise  the state of a flow
+   * @throws {string} throws missing tx id if called without a transaction to monitor
+   * @throws {Response} throws response object if the getFlowState request fails 
+   * @returns {Promise} returns a promise that resolves when txId's state is equal to 'delegated'
    */
-  async function waitForDelegation(transaction, resolve) {
-    if (!transaction) throw Error('missing tx id');
+  async function waitForDelegation(txId, resolve) {
+    if (!txId) throw Error('missing tx id');
 
+    // if there's no resolve parameter it's not initialized yet
+    // return a Promise recursively calls this function w/txId and resolve
     if (!resolve) return new Promise((resolve, reject) => {
-      waitForDelegation(transaction, resolve)
-    })
+      waitForDelegation(txId, resolve);
+    });
 
-    // const result = await fetch(`/api/getFlowState?flow_id=${transaction}`);
-    if (!result.ok) alert('error');
-    const { state } = await result.json();
+    const response = await fetch(`/api/getFlowState?flow_id=${txId}`);
+    if (!response.ok) throw new Error(response);
+    const { state } = await response.json();
     if (state === 'delegated') return resolve();
 
-    else return setTimeout(() => {
-      waitForDelegation(transaction, resolve);
+    // flow state isn't delegated, try again in 5 seconds
+    return setTimeout(() => {
+      waitForDelegation(txId, resolve);
     }, 5000);
   }
 
+  // on initial load update the balance
   useEffect(() => {
-    if (loaded && accountId) updateBalances()
-  }, [loaded, accountId])
+    if (loaded && accountId) updateBalances();
+  }, [loaded, accountId]);
 
+  // these components are defined in the /components directory
   return (
     <>
       <MetaData />
