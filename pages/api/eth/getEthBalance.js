a28e38a9f0b825190edb03c6f62c5171ea744a21
Rogan X <rogan@figment.io>
2023-01-09 16:00:52 -0700

    Use .env

--- pages/api/eth/getEthBalance.js
+++ pages/api/eth/getEthBalance.js
@@ -3,7 +3,7 @@
 const { ethers, utils } = require("ethers");
 
 async function getEthBalance() {
-    const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/HazH9PhOY6iI8e9mCeJPCKP6fkQT2NzI");
+    const provider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
 
     const signer = provider.getSigner()
     
